6d0724bda15cfb2a0ea7a1c311523ad82b10b209
Alexis Toulotte <al@alweb.org>
2009-01-28 10:48:11 +0100

    Added redirection loop check

--- lib/cas_client/request.rb
+++ lib/cas_client/request.rb
@@ -4,20 +4,26 @@ module CasClient
     
     include CasClient::Logger
     
+    MAX_REDIRECTIONS = 5 unless const_defined?(:MAX_REDIRECTIONS)
+    
+    attr_reader :params
     attr_reader :provider
     attr_reader :service_url
-    attr_reader :ticket
     
     def initialize(service_url, params = {}, provider = ServiceProvider::Base.new)
+      self.service_url = service_url
+      @params = params.with_indifferent_access
       @provider = provider
-      @service_url = service_url
-      @ticket = params['ticket']
+      raise CasClient::Error.new("Max redirections: #{MAX_REDIRECTIONS}") if redirection_count > MAX_REDIRECTIONS
     end
     
     # Available options are: :renew (default: false)
     def login_url(options = {})
       returning(provider.login_url) do |url|
-        url.query = "service=#{CGI.escape(service_url)}"
+        service = service_url.dup
+        service.query << '&' if service.query
+        service.query = "#{service.query}redirection_count=#{redirection_count + 1}"
+        url.query = "service=#{CGI.escape(service.to_s)}"
         url.query << '&renew=true' if options[:renew]
       end
     end
@@ -26,6 +32,10 @@ module CasClient
       returning(provider.logout_url) do |url|
         url.query = "destination=#{CGI.escape(destination)}" unless destination.blank?
       end
+    end
+    
+    def ticket
+      params[:ticket]
     end
     
     def validable?
@@ -53,6 +63,18 @@ module CasClient
       end
     rescue Timeout::Error, StandardError => e
       raise CasClient::Error.new(e, url)
+    end
+    
+    private
+    
+    def redirection_count
+      params[:redirection_count].to_i
+    end
+    
+    def service_url=(url)
+      @service_url = url.is_a?(URI) ? url : URI.parse(url)
+    rescue => e
+      raise CasClient::Error.new(e)
     end
     
   end
--- spec/cas_client/request_spec.rb
+++ spec/cas_client/request_spec.rb
@@ -4,12 +4,12 @@ describe CasClient::Request do
   
   it 'returns login url' do
     request = CasClient::Request.new('http://example.com')
-    request.login_url.should == URI.parse('http://localhost:3001/cas/login?service=http%3A%2F%2Fexample.com')
+    request.login_url.should == URI.parse('http://localhost:3001/cas/login?service=http%3A%2F%2Fexample.com%3Fredirection_count%3D1')
   end
   
   it 'returns login url with renew parameter' do
     request = CasClient::Request.new('http://example.com')
-    request.login_url(:renew => true).should == URI.parse('http://localhost:3001/cas/login?service=http%3A%2F%2Fexample.com&renew=true')
+    request.login_url(:renew => true).should == URI.parse('http://localhost:3001/cas/login?service=http%3A%2F%2Fexample.com%3Fredirection_count%3D1&renew=true')
   end
   
   it 'returns logout url' do
@@ -22,9 +22,30 @@ describe CasClient::Request do
     request.logout_url('http://example.net').should == URI.parse('http://localhost:3001/cas/logout?destination=http%3A%2F%2Fexample.net')
   end
   
+  it 'increments redirection_count parameter' do
+    request = CasClient::Request.new('http://example.com?bar=baz', :redirection_count => 3)
+    request.login_url.should == URI.parse('http://localhost:3001/cas/login?service=http%3A%2F%2Fexample.com%3Fbar%3Dbaz%26redirection_count%3D4')
+  end
+  
+  it 'must have a valid service url' do
+    lambda {
+      CasClient::Request.new('http://')
+    }.should raise_error(CasClient::Error)
+  end
+  
+  it 'raise if redirection_count is greater than 5' do
+    lambda {
+      CasClient::Request.new('http://example.com', :redirection_count => 5)
+    }.should_not raise_error
+    
+    lambda {
+      CasClient::Request.new('http://example.com', :redirection_count => 6)
+    }.should raise_error(CasClient::Error, 'Max redirections: 5')
+  end
+  
   it 'use ticket request parameter' do
-    CasClient::Request.new('http://example.com', 'bar' => 'foo').should_not be_validable
-    CasClient::Request.new('http://example.com', 'ticket' => 'foo').should be_validable
+    CasClient::Request.new('http://example.com', :bar => 'foo').should_not be_validable
+    CasClient::Request.new('http://example.com', :ticket => 'foo').should be_validable
   end
   
 end
