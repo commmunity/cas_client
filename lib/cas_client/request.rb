114ab12a06314438c24d5709ddd26247cd8f2925
Alexis Toulotte <alexis.toulotte@feedback20.com>
2009-04-28 19:31:37 +0200

    using rack if defined

--- lib/cas_client/request.rb
+++ lib/cas_client/request.rb
@@ -42,7 +42,26 @@ module CasClient
       params[:ticket]
     end
     
-    def validate(options = { :timeout => 5 })
+    def validate(options = { :timeout => 15 })
+      defined?(CasServer::Rack::Api::ServiceValidate) ? validate_with_rack : validate_with_http(options)
+    rescue Timeout::Error, StandardError => e
+      raise CasClient::Error.new(e)
+    end
+    
+    private
+    
+    def service_url=(url)
+      @service_url = url.is_a?(URI) ? url : URI.parse(url)
+      @service_url.fragment = nil
+      if @service_url.query.present?
+        @service_url.query.gsub!(/[\&]?ticket=[^\&]*\&?/, '')
+        @service_url.query = nil if @service_url.query.empty?
+      end
+    rescue => e
+      raise CasClient::Error.new(e)
+    end
+    
+    def validate_with_http(options)
       url = provider.url_for(:validate)
 
       logger.debug("[CAS] Posting request to: #{url}")
@@ -72,21 +91,12 @@ module CasClient
           return CasClient::Response::Base.parse(response.body)
         end
       end
-    rescue Timeout::Error, StandardError => e
-      raise CasClient::Error.new(e, url)
     end
     
-    private
-    
-    def service_url=(url)
-      @service_url = url.is_a?(URI) ? url : URI.parse(url)
-      @service_url.fragment = nil
-      if @service_url.query.present?
-        @service_url.query.gsub!(/[\&]?ticket=[^\&]*\&?/, '')
-        @service_url.query = nil if @service_url.query.empty?
-      end
-    rescue => e
-      raise CasClient::Error.new(e)
+    def validate_with_rack(options = {})
+      url = provider.url_for(:validate, :service => service_url.to_s, :ticket => ticket)
+      body = Rack::MockRequest.new(CasServer::Rack::Api::ServiceValidate.new).get(url.to_s).body
+      CasClient::Response::Base.parse(body)
     end
     
   end
